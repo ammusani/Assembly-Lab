a863e3b5a6b664956caa8da87ee486e8b2d8235a
ammusani <33342567+ammusani@users.noreply.github.com>
2019-01-24 00:12:37 +0530

    Update swap.s

--- lab 1/swap.s
+++ lab 1/swap.s
@@ -6,24 +6,24 @@
 main:
 .LFB0:
 	.cfi_startproc
-	push	rbp
+	push	rbp	; stack[top-0] <- [rbp]
 	.cfi_def_cfa_offset 16
 	.cfi_offset 6, -16
-	mov	rbp, rsp
+	mov	rbp, rsp	; [rbp] <- [rsp]
 	.cfi_def_cfa_register 6
-	mov	DWORD PTR [rbp-8], 90
-	mov	DWORD PTR [rbp-4], 80
-	mov	eax, DWORD PTR [rbp-4]
-	add	DWORD PTR [rbp-8], eax
-	mov	eax, DWORD PTR [rbp-4]
-	mov	edx, DWORD PTR [rbp-8]
-	sub	edx, eax
-	mov	eax, edx
-	mov	DWORD PTR [rbp-4], eax
-	mov	eax, DWORD PTR [rbp-4]
-	sub	DWORD PTR [rbp-8], eax
-	mov	eax, 0
-	pop	rbp
+	mov	DWORD PTR [rbp-8], 90	;[rbp-8] <- 90
+	mov	DWORD PTR [rbp-4], 80	;[rbp-4] <- 80
+	mov	eax, DWORD PTR [rbp-4]	;eax <- [rbp-4] 
+	add	DWORD PTR [rbp-8], eax	;[rbp-8] <- eax
+	mov	eax, DWORD PTR [rbp-4]	;eax <- [rbp-4]
+	mov	edx, DWORD PTR [rbp-8]	;edx <- [rbp-8]
+	sub	edx, eax		;edx <- eax
+	mov	eax, edx		;eax <- edx
+	mov	DWORD PTR [rbp-4], eax	;[rbp-4] <- eax; [rbp-4] = 90 
+	mov	eax, DWORD PTR [rbp-4]	;eax <- [rbp-4]
+	sub	DWORD PTR [rbp-8], eax	;[rbp-8] <- eax; [rbp-8] = 80
+	mov	eax, 0			;eax <- 0
+	pop	rbp			;rbp <- stack[top -0];
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
