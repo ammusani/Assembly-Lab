68091d56ec851ee7ac9d2cf72d223e023635860b
ammusani <33342567+ammusani@users.noreply.github.com>
2019-01-25 16:56:11 +0530

    Add files via upload

---
+++ arith.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+	int a = 90;
+	int b = 10;
+	int c = a + b;
+	int d = a - b;
+	int e = a * b;
+	int f = a / b;
+	int g = a & b;
+	int h = a | b;
+	int i = a ^ b;
+	return 0;
+}
