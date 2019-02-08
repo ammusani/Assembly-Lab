06e35e9aed2ba3958c8f18e06eba573c18a47f49
ammusani <33342567+ammusani@users.noreply.github.com>
2019-02-08 18:04:57 +0530

    Update file_4_2.asm

--- lab3/file_4_2.asm
+++ lab3/file_4_2.asm
@@ -1,6 +1,8 @@
 main:
 	add $s0, $zero, 35
 	add $s1, $zero, 12
-
-	xor $s0, $s0, $s0
-	mul $s1, $s1, -1
+	add $s2, $zero, 0xABCD
+	
+	xor $s0, $s0, $s0		#making number zero using XOR
+	mul $s1, $s1, -1		#2's complement without adding 1
+	xor $s2, $s2, 0xFFFF		#1's complement
