060a292ef4450423065189da664437dd0c1e5d6f
ammusani <33342567+ammusani@users.noreply.github.com>
2019-02-15 17:43:19 +0530

    Add files via upload

---
+++ lab4_1_a.asm
@@ -0,0 +1,23 @@
+main:
+
+	addiu $s0, $zero, 32
+	li $s1, 0xAAAA
+	addi $t1, $zero, 32
+	addiu $t2, $zero, 1
+	addiu $t3, $zero, 0
+	addiu $t4, $zero, 0
+
+P:	beq $t1, $zero, EXIT
+	and $t5, $s1, $t2
+	slt $t6, $zero, $t5
+	beq $t6, $zero, Q
+	addiu $t4, $t4, 1
+	j R
+
+Q:	addiu $t3, $t3, 1
+
+R:	sll $t2, $t2, 1
+	addi $t1, $t1, -1
+	j P
+EXIT:	sw $t3, 0x10000000
+	sw $t4, 0x10000004 
